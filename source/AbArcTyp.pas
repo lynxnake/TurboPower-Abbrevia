67fb54b4d66f340f5f38968baaefee358bbc1265
robertjlove <robertjlove@89253570-fd12-4709-a508-8497ac4b1c7b>
2003-09-14 17:25:16 +0000

    Fix for [ 806077 ] and [ 785769 ] Same fix for both bugs.
    git-svn-id: https://svn.code.sf.net/p/tpabbrevia/code/trunk@42 89253570-fd12-4709-a508-8497ac4b1c7b

--- source/AbArcTyp.pas
+++ source/AbArcTyp.pas
@@ -871,10 +871,18 @@ begin
   if AbDriveIsRemovable(FileName) then
     FStream := TAbSpanStream.Create(FileName, Mode, mtRemoveable, FSpanningThreshold)
   else
-    FStream := TAbSpanStream.Create(FileName, Mode, mtLocal, FSpanningThreshold);
+//   FStream := TAbSpanStream.Create(FileName, Mode, mtLocal, FSpanningThreshold);
+// !!.05 The following is a test to use TFileStream directly if not spanning
+// This removes the headaches of not being able to seek() when not spanning.
+// Allowing features such as TestTagged, and Add then Extract to work without
+// having to close reopen the archive.
+   FStream := TFileStream.Create(FileName,Mode);
 
-  TAbSpanStream(FStream).OnRequestImage := DoSpanningMediaRequest;
-  TAbSpanStream(FStream).OnArchiveProgress := DoArchiveSaveProgress;   {!!.04}
+  if (FStream is TAbSpanStream) then {!!.05}
+   begin
+    TAbSpanStream(FStream).OnRequestImage := DoSpanningMediaRequest;
+    TAbSpanStream(FStream).OnArchiveProgress := DoArchiveSaveProgress;   {!!.04}
+   end;
 
   FLogStream := nil;
   FOwnsStream := True;
@@ -1795,8 +1803,8 @@ var
   Confirm : Boolean;
 begin
   if Status = asInvalid then
-    Exit;                                                              
-  if (not FIsDirty) and (Count > 0) then                             
+    Exit;
+  if (not FIsDirty) and (Count > 0) then
     Exit;
   Lock;
   try
